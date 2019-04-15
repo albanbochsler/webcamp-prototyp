def7de6e3bb4c12a5a4034af1b0b5fbbe91a7b7d
Urs Breton <ub@feinheit.ch>
2019-04-15 15:20:38 +0200

    added mobile upload

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -51,26 +51,26 @@ function hasGetUserMedia() {
 (function (logger) {
   console.old = console.log;
   console.log = function () {
-    var output = "", arg, i;
+    var output = '', arg, i;
 
     for (i = 0; i < arguments.length; i++) {
       arg = arguments[i];
-      output += "<span class=\"log-" + (typeof arg) + "\">";
+      output += '<span class="log-' + (typeof arg) + '">';
 
       if (
-        typeof arg === "object" &&
-        typeof JSON === "object" &&
-        typeof JSON.stringify === "function"
+        typeof arg === 'object' &&
+        typeof JSON === 'object' &&
+        typeof JSON.stringify === 'function'
       ) {
         output += JSON.stringify(arg);
       } else {
         output += arg;
       }
 
-      output += "</span>&nbsp;";
+      output += '</span>&nbsp;';
     }
 
-    logger.innerHTML += output + "<br>";
+    logger.innerHTML += output + '<br>';
     console.old.apply(undefined, arguments);
   };
-})(document.getElementById("logger"));
+})(document.getElementById('logger'));
